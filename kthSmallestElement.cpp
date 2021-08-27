67bec9fd9850a25ea15154d2ec99c54df1d616c2
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-08-27 23:17:54 +0530

    Create kthSmallestElement.cpp

---
+++ kthSmallestElement.cpp
@@ -0,0 +1,78 @@
+/*
+
+Question Link - https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
+Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        int i;
+        
+        priority_queue<int , vector<int>,greater<int>> pq;
+        
+        for(i=l;i<r+1;i++)
+            pq.push(arr[i]);
+        
+        int k_small_num,count=1;
+        while(!pq.empty())
+        {
+            if(count == k)
+            {
+                k_small_num = pq.top();
+                break;
+            }
+            count++;
+            pq.pop();
+        }
+        return k_small_num;
+            
+    }
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
