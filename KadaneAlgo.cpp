bf1efce8c14be2bc9f53d020a3a96c75da8aed41
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-08-30 21:51:57 +0530

    Create KadaneAlgo.cpp

---
+++ KadaneAlgo.cpp
@@ -0,0 +1,68 @@
+/*
+
+Link - https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int i,meh=0,msf=0;
+        for(i=0;i<n;i++)
+        {
+            meh = meh + arr[i];
+            if(arr[i] > meh)
+                meh = arr[i];
+            if(msf < meh)
+                msf = meh;
+        }
+        return msf;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
